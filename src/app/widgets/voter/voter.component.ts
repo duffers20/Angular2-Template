a9b15f7c538c3c0618bca3a0f920245c397eef95
Gareth Oates <goatie@gmail.com>
2016-09-20 17:44:39 +0200

    Added VendorComponent (#13)
    Everything looks good to me - we could maybe come up with a simple checklist of things to check when reviewing each other's changes.

---
+++ src/app/widgets/voter/voter.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'ui-voter',
+    templateUrl: './voter.component.html',
+    styleUrls: ['./voter.component.css']
+})
+export class VoterComponent {
+
+    @Input() voteCount: number;
+    @Input() myVote: number;
+
+    UpVote() {
+        if (this.myVote === 1) { return; };
+            this.myVote++;
+    }
+
+    DownVote() {
+        if (this.myVote === -1) { return; };
+            this.myVote--;
+    }
+}
