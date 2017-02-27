4f59a04e9f85e289d3c2cef264de58a1076d408a
James Duffield <James.Duffield@genesisoilandgas.com>
2017-02-27 11:20:11 +0000

    Updated package versions.

--- src/systemjs.config.js
+++ src/systemjs.config.js
@@ -13,14 +13,14 @@
       // our app is within the app folder
       app: 'app',
       // angular bundles
-      '@angular/core': 'vendor:@angular/core/bundles/core.umd.js',
       '@angular/common': 'vendor:@angular/common/bundles/common.umd.js',
       '@angular/compiler': 'vendor:@angular/compiler/bundles/compiler.umd.js',
+      '@angular/core': 'vendor:@angular/core/bundles/core.umd.js',
+      '@angular/forms': 'vendor:@angular/forms/bundles/forms.umd.js',
+      '@angular/http': 'vendor:@angular/http/bundles/http.umd.js',
       '@angular/platform-browser': 'vendor:@angular/platform-browser/bundles/platform-browser.umd.js',
       '@angular/platform-browser-dynamic': 'vendor:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-      '@angular/http': 'vendor:@angular/http/bundles/http.umd.js',
       '@angular/router': 'vendor:@angular/router/bundles/router.umd.js',
-      '@angular/forms': 'vendor:@angular/forms/bundles/forms.umd.js',
       // other libraries
       'rxjs': 'vendor:rxjs'
     },
